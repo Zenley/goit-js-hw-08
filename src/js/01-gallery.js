98205dac57654f380ba0bd1c2626f481b1004078
Zenley <valy468@gmail.com>
2024-07-19 23:32:00 +0300

    .
    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox'
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const theGallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`;
+    }).join('');
+}
+
+theGallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const imageGallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VID_CURRENT_WATCHING_TIME = 'where-you-left';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem(VID_CURRENT_WATCHING_TIME, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const storedTime = localStorage.getItem(VID_CURRENT_WATCHING_TIME);
+if (storedTime) {
+  player.setCurrentTime(JSON.parse(storedTime));
+} else {
+  console.log('No stored playback time found.');
+}
