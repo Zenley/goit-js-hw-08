8edf08de3972b16a4c2a62168d642a77dcd89e80
Zenley <valy468@gmail.com>
2024-07-21 16:47:19 +0300

    .

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from "lodash.throttle" 
+
+const OBJECT_KEY = 'feedback-form-state'
+const feedback = document.querySelector('.feedback-form');
+const { email, message } = feedback.elements;
+
+feedback.addEventListener('input', throttle(onInputData, 500));
+feedback.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(OBJECT_KEY)) || {};
+pageRefresh();
+
+function onInputData(event) {
+    dataForm = {
+        email: email.value,
+        message: message.value
+    };
+    localStorage.setItem(OBJECT_KEY, JSON.stringify(dataForm))
+}
+
+function pageRefresh() {
+    if (dataForm) {
+        email.value = dataForm.email || '',
+        message.value = dataForm.message || ''
+    }
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (email.value === '' || message.value === '') {
+        return alert('Double check if all rows have been filled.');
+    }
+console.log({ email: email.value, message: message.value });
+
+localStorage.removeItem(OBJECT_KEY);
+feedback.reset();
+dataForm = {};
+
+}
 
